668b64ba522d6548cca45a3b64c90bdd2be28c21
StygianEclipse <nicolae.codleanu@oracle.com>
2026-03-06 14:47:47 +0200

    Updated to work with gradle build

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -69,7 +69,7 @@ const HomeScreen: React.FC = () => {
         <Calendar
           onDayPress={onDayPress}
           markedDates={marked}
-          markingType=\"multi-dot\"
+          markingType="multi-dot"
           current={selectedDate}
           enableSwipeMonths
           theme={calendarTheme}
